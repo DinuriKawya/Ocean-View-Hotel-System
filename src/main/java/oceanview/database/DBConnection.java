c7c1042cbc1750f020775c5fa50668d998223315
DinuriKawya <dinurikawya5@gmail.com>
2026-02-28 20:24:47 +0530

    Save local changes before pulling

--- src/main/java/oceanview/database/DBConnection.java
+++ src/main/java/oceanview/database/DBConnection.java
@@ -8,7 +8,7 @@ public class DBConnection {
 	
 	private static final String URL = "jdbc:mysql://localhost:3306/oceanview_db";
 	private static final String USER = "root";
-	private static final String PASSWORD = "1313";
+	private static final String PASSWORD = "admin123";
 	
 	public static Connection getConnection() throws SQLException {
 		try {
