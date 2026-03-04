75d1f4b5a89749ae012527653073e4c32b3a793c
DinuriKawya <dinurikawya5@gmail.com>
2026-03-05 05:00:25 +0530

    Reservation collection codes adding
    Signed-off-by: DinuriKawya <dinurikawya5@gmail.com>

--- src/main/java/oceanview/factory/AbstractRoom.java
+++ src/main/java/oceanview/factory/AbstractRoom.java
@@ -6,28 +6,52 @@ import oceanview.model.RoomType;
 
 public abstract class AbstractRoom {
 
-    protected Room room;  
+    protected Room room;   // AbstractRoom "has-a" Room
 
     public AbstractRoom() {
         this.room = new Room();
         configureRoom();
     }
 
-    protected abstract void configureRoom();
+    protected abstract void configureRoom(); //abstract method
 
-    public Room getRoom() { return room; }
+    public Room getRoom() {
+        return room;
+    }
 
-    public int getRoomNumber() { return room.getRoomNumber(); }
-    public void setRoomNumber(int number) { room.setRoomNumber(number); }
+    public int getRoomNumber() {
+        return room.getRoomNumber();
+    }
 
-    public double getPricePerNight() { return room.getPricePerNight(); }
-    public void setPricePerNight(double price) { room.setPricePerNight(price); }
+    public void setRoomNumber(int number) {
+        room.setRoomNumber(number); 
+    }
 
-    public int getFloor() { return room.getFloor(); }
-    public void setFloor(int floor) { room.setFloor(floor); }
+    public double getPricePerNight() {
+        return room.getPricePerNight(); // delegation
+    }
 
-    public RoomStatus getStatus() { return room.getStatus(); }
-    public void setStatus(RoomStatus status) { room.setStatus(status); }
+    public void setPricePerNight(double price) {
+        room.setPricePerNight(price);
+    }
 
-    public RoomType getRoomType() { return room.getRoomType(); }
+    public int getFloor() {
+        return room.getFloor();
+    }
+
+    public void setFloor(int floor) {
+        room.setFloor(floor);
+    }
+
+    public RoomStatus getStatus() {
+        return room.getStatus();
+    }
+
+    public void setStatus(RoomStatus status) {
+        room.setStatus(status);
+    }
+
+    public RoomType getRoomType() {
+        return room.getRoomType();
+    }
 }
